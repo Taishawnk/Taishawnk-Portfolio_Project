192177c101d063857e5cfff61873be5e8ddec890
Taishawnk <53539076+Taishawnk@users.noreply.github.com>
2023-06-01 23:13:40 -0500

    updated-styles-and-signin

--- projectfrontend/src/core/Menu.js
+++ projectfrontend/src/core/Menu.js
@@ -1,24 +1,31 @@
 import React from "react";
-
-
-
-
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 const Menu = () => {
-    return(
-        <div className="nav-bar-wrapper">
-            <ul className="inner-nav nav-tabs bg-dark">
-                <li className="nav-item text-white">
-                    <a className="nav-link" href="#">Home</a>
-                </li>
-                <li className="nav-item text-white">
-                    sign In
-                </li>
-            </ul>
+  const CurrentTab = (path) => {
+    const location = useLocation();
+    if (location.pathname === path) {
+      return { color: "CEC0FC" };
+    } else {
+      return { color: "#FFFFFF" };
+    }
+  };
 
-        </div>
-    )
-}
+  return (
+    <div className="nav-bar-wrapper">
+      <ul className="inner-nav nav-tabs bg-dark">
+        <li className="navz nav-item text-white" style={CurrentTab("/")}>
+          <Link  className="inner-navz nav-link" to="/">Home</Link>
+        </li>
+        <li className="navz nav-item text-white" tabIndex={1} style={CurrentTab("/signin")}>
+          <Link  className="inner-navz nav-link" to="/signin">signin</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Menu;
 
 
-export default Menu
